517643ea92b8a66f60e5a289a0a5f518fe769263
ryo-i <iidazz0109@gmail.com>
2024-06-01 18:31:02 +0900

    ユーザーをデータベースから見つけて検証する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
     render 'new', status: :unprocessable_entity
   end
 
