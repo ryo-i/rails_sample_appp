cdb4d2ee7b4f03f98646ec95dfe169abfcd868aa
ryo-i <iidazz0109@gmail.com>
2024-06-21 07:03:06 +0900

    development環境のメール設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,14 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # host = 'example.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  # config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+  # localhostで開発している場合は以下をお使いください
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
